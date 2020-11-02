60bf1159ec57d460fa036e18df973bdce302b51c
John Vasilakis <johnvslk@gmail.com>
2020-11-02 22:52:03 +0200

    Initiating downloader program

---
+++ download.py
@@ -0,0 +1,14 @@
+"""
+Downloader
+"""
+#libraries
+
+#url
+
+#cehck if file is created
+
+#if not create it
+
+#if yes don't do anything
+
+#download the file and set the destination as dataset inside this repo
