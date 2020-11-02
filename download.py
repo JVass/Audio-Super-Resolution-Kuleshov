66f9ef5fdb4710bec98e3ca82167e9dd1800e43c
John Vasilakis <johnvslk@gmail.com>
2020-11-02 23:03:55 +0200

    Setup url and the chunking downloading procedure

--- download.py
+++ download.py
@@ -2,8 +2,10 @@
 Downloader
 """
 #libraries
+import requests
 
 #url
+url = "http://datashare.is.ed.ac.uk/download/DS_10283_3443.zip"
 
 #cehck if file is created
 
@@ -12,3 +14,8 @@ Downloader
 #if yes don't do anything
 
 #download the file and set the destination as dataset inside this repo
+r = requests.get(url, Stream = True)
+with open("dataset/VCTK.zip", "wb") as dataset:
+    for chunk in r.iter_content(chunk_size=1024)
+        if chunk:
+            dataset.write(chunk)
