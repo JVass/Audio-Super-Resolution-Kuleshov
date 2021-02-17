74c8e4e6ae3d6370f1d21d4f4f55d9800ca14f9c
unknown <yannis.vasilakis@athenarc.gr>
2021-02-17 21:30:54 +0200

    Due to dynamically chosen length of Down and Up blocks, the method withouth the keras API is prefered over the API.

--- architecture_obj.py
+++ architecture_obj.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from tensorflow.keras.layers import Dense, Conv1D, Dropout, BatchNormalization, MaxPooling1D, ReLU
+from tensorflow.keras.layers import Input, Dense, Conv1D, Dropout, BatchNormalization, MaxPooling1D, ReLU, concatenate
 from tensorflow.keras.layers.advance_activations import LeakyReLU
 from tensorflow.keras.initializations import normal, orthogonal
 
@@ -38,177 +38,74 @@ def LSD(y_true, y_pred):
     
     return tf.math.reduce_mean(tf.math.sqrt(inside_term))
 
-class AudioUnet(tf.keras.Model):
-    def __init__(self, ):
-        super(AudioUnet, self).__init__()
+def AudioUnet(L, ndim):
+    #Filter number and kernel sizes for L=1:8
+    n_filters = [128, 384, 512, 512, 512, 512, 512, 512]
+    n_filtersizes = [65, 33, 17, 9, 9, 9, 9, 9, 9]
 
-        n_filters = [128, 384, 512, 512, 512, 512, 512, 512]
-        n_filtersizes = [65, 33, 17, 9, 9, 9, 9, 9, 9]
+    init_input = Input(shape = (ndim,1))
 
-        self.downsampling = []
-        self.bottleneck = []
-        self.upsampling = []
+    downsampling_outputs = []
 
-        #Downsampling block components
-        for l, nf, nfs in zip(range(L), n_filters, n_filtersizes):
-            self.downsampling.append(Conv1D(filters = nf,
-                                            kernel_size = nfs,
-                                            padding = "same",
-                                            kernel_initializer = "orthogonal"))
-            self.downsampling.append(MaxPooling1D())
-            self.dowmsampling.append(LeakyReLU(0.2))
+    #downsampling blocks
+    x = Conv1D(filters = nf,
+                kernel_size = nfs,
+                padding = "same",
+                kernel_initializer = "orthogonal")(init_input)
+    x = MaxPooling1D()(x)
+    x = LeakyReLU(0.2)(x)
+    downsampling_outputs.append(x)
 
-        #Bottleneck component
-        self.bottleneck.append(Conv1D(filters = n_filters[-1],
-                                        kernel_size = n_filtersizes[-1],
-                                        padding = "same",
-                                        kernel_initializer = "orthogonal"))
-        self.bottleneck.append(MaxPooling1D())
-        self.bottleneck.append(Dropout(0.5))
-        self.bottleneck.append(LeakyReLU(0.2))
-                                        
-        #Upsampling component
-        for l, nf, nfs in reversed(range(L), n_filters, n_filtersizes):
-            self.upsampling.append(Conv1D(filters = nf,
-                                            kernel_size = nfs,
-                                            padding = "same",
-                                            kernel_initializer = "orthogonal"))
-            self.upsampling.append(Dropout(0.5))
-            self.upsampling.append(ReLU())
-            self.upsampling.append(SubPixl1D())                                            
+    for l in range(1,L):
+        x = Conv1D(filters = nf,
+                kernel_size = nfs,
+                padding = "same",
+                kernel_initializer = "orthogonal")(x)
+        x =  MaxPooling1D()(x)
+        x = LeakyReLU(0.2)(x)
+        downsampling_outputs.append(x)
 
-    def create_downsampling_block(self, x):
-        x = 
+    #bottleneck block
+    x = Conv1D(filters = n_filters[-1],
+                kernel_size = n_filtersizes[-1],
+                padding = "same",
+                kernel_initializer = "orthogonal")(x)
+    x = MaxPooling1D()(x)
+    x = Dropout(0.5)(x)
+    x = LeakyReLU(0.2)(x)
 
-    def create_bottleneck_block(self):
-        pass
+    #upsampling blocks
+    for l, nf, nfs, l_in in reversed(zip(range(L), n_filters, n_filtersizes, downsampling)):
+        x = Conv1D(filters = nf,
+                    kernel_size = nfs,
+                    padding = "same",
+                    kernel_initializer = "orthogonal")(x)
+        x = Dropout(0.5)(x)
+        x = ReLU()(x)
+        x = SubPixel1D(x)
+        x = concatenate(x, downsampling[l])
 
-    def create_upsampling_block(self):
-        pass
-    
-    def SubPixel1D(I, r=2):
-        """One-dimensional subpixel upsampling layer
-        Calls a tensorflow function that directly implements this functionality.
-        We assume input has dim (batch, width, r).
-        Works with multiple channels: (B,L,rC) -> (B,rL,C)
-        """
-        with tf.name_scope('subpixel'):
-            _, w, rc = I.get_shape()
-            assert rc % r == 0
-            c = rc / r
-            X = tf.transpose(I, [2,1,0]) # (rc, w, b)
-            X = tf.batch_to_space(X, [r], [[0,0]]) # (c, r*w, b)
-            X = tf.transpose(X, [2,1,0])
-            return X
+    #final convolution layer
+    x = Conv1D(filters = 2,
+                kernel_size = 9,
+                padding = "same",
+                kernel_initializer = "normal")
 
-    def call(self, inputs):
-        D1_conv = tf.keras.layers.Conv1D(filters = 128,
-                                        kernel_size = 65,
-                                        strides = 2,
-                                        padding = "same")(inputs)
+    x = merge([x, init_input])
 
-        D1_LeakyRelu = tf.keras.layers.LeakyReLU(alpha = 0.2)(D1_conv)
+    return tf.keras.Model(input = init_input, outputs = x)
 
-        D2_conv = tf.keras.layers.Conv1D(filters = 384,
-                                        kernel_size = 33,
-                                        strides = 2,
-                                        padding = "same")(D1_LeakyRelu)
-
-        D2_LeakyRelu = tf.keras.layers.LeakyReLU(alpha = 0.2)(D2_conv)               
-
-        D3_conv = tf.keras.layers.Conv1D(filters = 512,
-                                        kernel_size = 17,
-                                        strides = 2,
-                                        padding = "same")(D2_LeakyRelu)
-
-        D3_LeakyRelu = tf.keras.layers.LeakyReLU(alpha = 0.2)(D3_conv)
-
-        D4_conv = tf.keras.layers.Conv1D(filters = 512,
-                                        kernel_size = 9,
-                                        strides = 2,
-                                        padding = "same")(D3_LeakyRelu)
-
-        D4_LeakyRelu = tf.keras.layers.LeakyReLU(alpha = 0.2)(D4_conv)
-
-        #bottleneck
-        Bottleneck = tf.keras.layers.Conv1D(filters = 512,
-                                            kernel_size = 9,
-                                            activation)(D4_LeakyRelu)
-        Bneck_dropout = tf.keras.layers.Dropout(0.5)(Bottleneck)
-        Bneck_LeakyReLU = tf.keras.layers.LeakyReLU(aplha = 0.2)(Bneck_dropout)
-
-        #In the figure there is a bottleneck layer but in the paper Kuleshov doesnt provide us with info
-
-        U1_conv = tf.keras.layers.Conv1D(filters = 512,
-                                        kernel_size = 9,
-                                        strides = 2,
-                                        activation = "relu",
-                                        padding = "same")(Bneck_LeakyReLU)
-
-        U1_dropout = tf.keras.layers.Dropout(rate = 0.4)(U1_conv)
-        U1_ReLU = tf.keras.layers.ReLU()(U1_dropout)
-        U1_DimShuffle = SubPixel1D(U1_ReLU)
-        U1_Stacking = tf.concat([U1_DimShuffle,D4_conv], 2)
-
-        U2_conv = tf.keras.layers.Conv1D(filters = 512,
-                                        kernel_size = 17,
-                                        strides = 2,
-                                        activation = "relu",
-                                        padding = "same")(U1_Stacking)
-
-        U2_dropout = tf.keras.layers.Dropout(rate = 0.4)(U2_conv)
-        U2_ReLU = tf.keras.layers.ReLU()(U2_dropout)
-        U2_DimShuffle = SubPixel1D(U2_ReLU)
-        U2_Stacking = tf.concat([U2_DimShuffle,D4_conv], 2)
-
-        U3_conv = tf.keras.layers.Conv1D(filters = 384,
-                                        kernel_size = 33,
-                                        strides = 2,
-                                        activation = "relu",
-                                        padding = "same")(U2_Stacking)
-
-        U3_dropout = tf.keras.layers.Dropout(rate = 0.4)(U3_conv)
-        U3_ReLU = tf.keras.layers.ReLU()(U3_dropout)
-        U3_DimShuffle = SubPixel1D(U3_ReLU)
-        U3_Stacking = tf.concat([U3_DimShuffle,D4_conv], 2)
-
-        U4_conv = tf.keras.layers.Conv1D(filters = 128,
-                                        kernel_size = 65,
-                                        strides = 2,
-                                        activation = "relu",
-                                        padding = "same")(U3_Stacking)
-
-        U4_dropout = tf.keras.layers.Dropout(rate = 0.4)(U4_conv)
-        U4_ReLU = tf.keras.layers.ReLU()(U4_dropout)
-        U4_DimShuffle = SubPixel1D(U4_ReLU)
-        U4_Stacking = tf.concat([out,D4_conv], 2)
-
-        out_conv = tf.keras.layers.Conv1D(filters = 2,
-                                        kernel_size = 9,
-                                        strides = 2,
-                                        padding = "same")(U4_Stacking)
-
-        out = SubPixel1D(out_conv)
-
-        out = tf.math.add(out,inputs)
-
-        return out
-
-#instantiate the model
-model = AudioUnet()
-
-#Model parameters
-EPOCHS = 400
-ADAM = tf.keras.optimizers.Adam(learning_rate = 0.0001)
-
-"""
-This will be the section that the data will be read.
-"""
-
-#testing vector - data
-data = tf.ones([1,6000,1])
-
-model.fit(optimizer = ADAM,
-            loss = tf.keras.metrics.RootMeanSquaredError,
-            metrics = [SNR, LSD]
-            )
+def SubPixel1D(I, r=2):
+    """One-dimensional subpixel upsampling layer
+    Calls a tensorflow function that directly implements this functionality.
+    We assume input has dim (batch, width, r).
+    Works with multiple channels: (B,L,rC) -> (B,rL,C)
+    """
+    with tf.name_scope('subpixel'):
+        _, w, rc = I.get_shape()
+        assert rc % r == 0
+        c = rc / r
+        X = tf.transpose(I, [2,1,0]) # (rc, w, b)
+        X = tf.batch_to_space(X, [r], [[0,0]]) # (c, r*w, b)
+        X = tf.transpose(X, [2,1,0])
+        return X
